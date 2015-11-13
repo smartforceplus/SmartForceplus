6e699922c1ae1ce21a73e8c82ecfb1abb2b1cad3
smartforceplus <agardiner@tagsbm.com>
2015-11-13 19:30:02 +1100

    Hamid commits

---
+++ openerp/addons/tag_project_tasks/__openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Tag Project/Task View",
+    'version': "1.0",
+    'author': 'TAG Small Biz Community',
+    'website': 'http://www.smallbiz.community',
+    'category': "Tools",
+    'data': ['task_view.xml','project_view.xml'
+    ],
+    'demo': [],
+    'depends': ['project'],
+    'installable': True,
+}

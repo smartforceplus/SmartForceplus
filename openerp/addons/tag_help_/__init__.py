2d0e791cfb36e27ec523c06ef4e61ee16af8916d
smartforceplus <agardiner@tagsbm.com>
2015-12-04 19:30:02 +1100

    Hamid commits

--- openerp/addons/tag_help_/__init__.py
+++
@@ -1,2 +0,0 @@
-#Attempting to write a custom module to create help menu and admin set up
-import tag_help
---
+++ openerp/addons/tag_hr_skill/models/hr_skill.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Copyright (C) 2013 Savoir-faire Linux (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from openerp import fields, models, api
+
+
+class Skill(models.Model):
+    _name = 'hr.skill'
+    _parent_store = True
+    _order = 'parent_left'
+
+    name = fields.Char('Name', required=True, translate=True)
+    active = fields.Boolean('Active', default=True)
+    documentation_link = fields.Char(string="Training Documentation")
+    parent_id = fields.Many2one('hr.skill', 'Parent', ondelete='cascade')
+    parent_left = fields.Integer('Parent Left', index=True)
+    parent_right = fields.Integer('Parent Right', index=True)
+    child_ids = fields.One2many('hr.skill', 'parent_id', 'Children')
+    employee_ids = fields.Many2many(
+        'hr.employee',
+        'skill_employee_rel',
+        'skill_id',
+        'employee_id',
+        'Employee(s)')
+
+    employee_comp = fields.One2many('hr.skill.comp','skill_id', string="Employees")
+    
+    @api.multi
+    def name_get(self):
+        res = []
+        for skill in self:
+            names = []
+            current = skill
+            while current:
+                names.append(current.name)
+                current = current.parent_id
+            res.append((skill.id, ' / '.join(reversed(names))))
+        return res
+
+class SkillComp(models.Model):
+    
+    _name = 'hr.skill.comp'
+    
+    skill_id = fields.Many2one('hr.skill',string="Skill")
+    employee_id = fields.Many2one('hr.employee', string="Employee")
+    comp = fields.Selection((('A','A'),('B','B'),('C','C'),('D','D'),('E','E')),string="Competency Level")
